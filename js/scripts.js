9ceae91599e63e34b29a73d3bd7ed5940f43ba78
brailynmcguire <mcguirebrailyn@gmail.com>
2026-04-07 10:02:47 -0400

    hw 4/7/26

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
-
-
+// copyright year
 window.onload = function () {
     document.getElementById("copyright").innerHTML = new Date().getFullYear();
 }
+
+// slideshow function
+let intIndex = 1
+function showImage(i) {
+    intIndex += i;
+
+    let theImages = document.getElementsByClassName("image");
+    let theDots = document.getElementsByClassName("dot");
+
+    for(n = 0; n < theImages.length; n++) {
+        theImages[n].style.display = "none";
+        theDots[n].className = theDots[n].className.replace(" active", "");
+        if (intIndex > theImages.length - 1) {
+            intIndex = 0;
+        }
+        if (intIndex < 0) {
+            intIndex = theImages.length - 1
+        }
+    }
+
+    theImages[intIndex].style.display = "block"
+    theDots[intIndex].className += " active"
+}
+
+setInterval(() => {
+    showImage(1)
+}, 5000);
+    
